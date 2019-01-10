0a939cb15f13b53af87922a3b5d0773f1d3721d4
Atqa Amir <amiratqa@gmail.com>
2019-01-10 12:14:59 +0500

    feat(ConfigReader): create data structure from all json

--- common/config_reader.py
+++ common/config_reader.py
@@ -2,17 +2,29 @@ import json
 
 
 class ConfigReader:
-    def __init__(self):
-        pass
+    __data = ""
+
+    def __init__(self, filename="base-configuration.json"):
+        """
+        Virtually private constructor
+        :param filename: json (base-config) containing basic config of simulation and all other json file names 
+        """
+        if ConfigReader.__data == "":
+            ConfigReader.__data = self.create_data(filename)
+        else:
+            raise Exception("Object already exists")
 
     @staticmethod
-    def create_data(filename):
+    def get_data(filename="base-configuration.json"):
+        if ConfigReader.__data == "":
+            ConfigReader(filename)
+        return ConfigReader.__data
+
+    def create_data(self, filename):
         """
         Iterates over file names in base-config and concatenates all data into one data structure
-        :param filename: name of file containing simulation data (base-config)
+        :param filename: json (base-config) containing basic config of simulation and all other json file names
         :return: concatenated data structure of all json files
-
-
         """
 
         with open("../data/configs/app_config/"+filename) as f:
