fix(Renderer): Fix car scaling

simulation/vehicle/traffic_creator.py
@@ -27,6 +27,9 @@ class TrafficCreator(object):
                 v = TrafficCreator.set_position(v, map1, taken)
                 TrafficCreator.__traffic.append(v)
                 ind_x += 1
+
+        for v in TrafficCreator.__traffic:
+            print("(x,y) ", v.x, "    ", v.y,"    ","back", v.back_point,"    ","front:", v.front_point)
         return TrafficCreator.__traffic
 
     @staticmethod
@@ -45,9 +48,10 @@ class TrafficCreator(object):
         lane_idx = None
         xy_id = None
         lane_points = []
+        car_length = v.car_length
 
         # choose a point where a car is not already present
-        while (tup in taken) or (_do is True):
+        while (TrafficCreator.__is_tuple_valid(tup, taken, lane_points, xy_id, car_length, map1, road_idx, lane_idx) is False) or (_do is True):
             _do = False
             road_idx = random.randint(0, len(map1.roads)-1)
             lane_idx = random.randint(0, len(map1.roads[road_idx].lanes)-1)
@@ -61,6 +65,11 @@ class TrafficCreator(object):
 
             xy_id = random.randint((v.car_length/2.0), (len(lane_points) - (v.car_length/2.0))-1)
             tup = (map1.roads[road_idx].road_id, map1.roads[road_idx].lanes[lane_idx].id, lane_points[xy_id][1])
+
+            print("----------------tupple in taken------------")
+            print("tup: ", tup)
+            print("taken: ", taken)
+            print("--------------------------------------------")
 
         lower_limit = lane_points[xy_id][1] - (v.car_length/2.0)
         upper_limit = lane_points[xy_id][1] + (v.car_length/2.0)
@@ -78,11 +87,33 @@ class TrafficCreator(object):
         v.lane_id = map1.roads[road_idx].lanes[lane_idx].id
         v.x = lane_points[xy_id][0]
         v.y = lane_points[xy_id][1]
-
+        print ("---------------xy-------------")
+        print ("x", v.x)
+        print("y",v.y)
+        print("-------------------------------")
         v.front_point = (v.x, upper_limit)
         v.back_point = (v.x, lower_limit)
-
+        print("----------vehicle setttttt---------------")
         return v
+
+    @staticmethod
+    def __is_tuple_valid(tup, taken, lane_points, xy_id, car_length, map1, road_idx, lane_idx):
+        _taken = taken.copy()
+        if (tup is not None) and (len(taken) != 0):
+            lower_limit = lane_points[xy_id][1] - (car_length / 2.0)
+            upper_limit = lane_points[xy_id][1] + (car_length / 2.0)
+
+            # taken points by this car
+            points = TrafficCreator.points_in_yrange(map1, road_idx, lane_idx, (lower_limit, upper_limit))
+
+            for p in points:
+                p_tup = (map1.roads[road_idx].road_id, map1.roads[road_idx].lanes[lane_idx].id, p[1])
+                print("POINTS: ", p_tup)
+                if p_tup in taken:
+                    return False
+            return True
+        else:
+            return True
 
     @staticmethod
     def vehicle_creator(percentage, type1):
@@ -116,7 +147,7 @@ class TrafficCreator(object):
         """
 
         coordinates = np.array([])
-        starting_position_x = lane_width*(lane_id-1) + (lane_width / 2)
+        starting_position_x = lane_width*(lane_id-1) + (lane_width / 2.0)
         starting_position_y = starting_position[1]
         bearing = deg2rad(bearing)
 