d0c71865e364b1670e73c5aa62b89c4ba5864537
Maira Farooq <maira.farooq.mf@gmail.com>
2019-01-22 00:01:20 +0500

    feat(RuleBased): implement move feature

--- simulation/vehicle/rule_based.py
+++ simulation/vehicle/rule_based.py
@@ -1,5 +1,4 @@
 from simulation.vehicle.vehicle import Vehicle
-import numpy as np
 from common.config_reader import ConfigReader
 from common.decisions import Decisions
 from common.utility import *
@@ -11,22 +10,24 @@ class RuleBased(Vehicle):
         super(RuleBased, self).__init__(perception_size, speed_limit, acceleration, de_acceleration, length, type1)
 
     def move(self, road_type, bearing, intercept, decision):
-
+        bearing = deg2rad(bearing)
         if Decisions[decision].value == Decisions.Accelerate.value:
-            self.__speed = (self.__acceleration * 1.0/ConfigReader.get_data("fps")) + self.__speed
-            self.__x = self.__x + self.__speed*np.cos(bearing)
-            self.__y = self.__y + self.__speed * np.sin(bearing)
+            self._Vehicle__speed = (self._Vehicle__acceleration * (1.0/ConfigReader.get_data("fps")[0])) + self._Vehicle__speed
+            self._Vehicle__x = self._Vehicle__x + self._Vehicle__speed * np.cos(bearing)
+            self._Vehicle__y = self._Vehicle__y + self._Vehicle__speed * np.sin(bearing)
 
         elif Decisions[decision].value == Decisions.Constant_speed.value:
-            # self.__speed = (self.__acceleration * 1.0/ConfigReader.get_data("fps")) + self.__speed
-            self.__x = self.__x + self.__speed*np.cos(bearing)
-            self.__y = self.__y + self.__speed * np.sin(bearing)
+            self._Vehicle__x = self._Vehicle__x + self._Vehicle__speed*np.cos(bearing)
+            self._Vehicle__y = self._Vehicle__y + self._Vehicle__speed * np.sin(bearing)
 
         elif Decisions[decision].value == Decisions.De_accelerate.value:
-            self.__speed = (self.de_acceleration * 1.0/ConfigReader.get_data("fps")) + self.__speed
-            self.__x = self.__x + self.__speed*np.cos(bearing)
-            self.__y = self.__y + self.__speed * np.sin(bearing)
+            self._Vehicle__speed = (self._Vehicle__de_acceleration * (1.0/ConfigReader.get_data("fps")[0])) + self._Vehicle__speed
+            self._Vehicle__x = self._Vehicle__x + self._Vehicle__speed*np.cos(bearing)
+            self._Vehicle__y = self._Vehicle__y + self.__speed * np.sin(bearing)
 
         elif Decisions[decision].value == Decisions.Move_right.value:
-            distance = point_to_line(RoadType[road_type].value, (self.__x, self.__y), bearing, intercept)
-            self.__x = self.__x + distance
+            distance = point_to_line(road_type, (self._Vehicle__x, self._Vehicle__y), bearing, intercept)
+            self._Vehicle__x = self._Vehicle__x + distance
+
+        self._Vehicle__front_point = (self._Vehicle__x, self._Vehicle__y + (self._Vehicle__car_length / 2.0))
+        self._Vehicle__back_point = (self._Vehicle__x, self._Vehicle__y - (self._Vehicle__car_length / 2.0))
