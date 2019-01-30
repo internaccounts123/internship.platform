c36d828d4541327f11a44d44b2fa164490d598b3
Asad Rizvi <asadimranrizvi@hotmail.com>
2019-01-30 12:42:49 +0500

    refactor(logger): removed redundancy

--- common/logging/logger.py
+++ common/logging/logger.py
@@ -31,38 +31,32 @@ class Logger:
 
     @staticmethod
     def get_logger(log_type):
-        if log_type == "FILE":
-            logger = logging.getLogger(__name__)
-            logger.setLevel(logging.INFO)
+        """
 
-            formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+        :param log_type: The type of logging the user has to use, to a file, to a console or both.
+        :return: logger as desired
+        """
+
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+
+        if log_type == "FILE":
             filename = "File_Log" + str(time.time())+".log"
             file_handler = logging.FileHandler(filename, mode='a')
             file_handler.setFormatter(formatter)
-
             logger.addHandler(file_handler)
             return logger
 
         if log_type == "CONSOLE":
-            logger = logging.getLogger(__name__)
-            logger.setLevel(logging.INFO)
-
-            formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
 
             stream_handler = logging.StreamHandler()
             stream_handler.setFormatter(formatter)
-
             logger.addHandler(stream_handler)
             return logger
 
         if log_type == "BOTH":
-            logger = logging.getLogger(__name__)
-            logger.setLevel(logging.INFO)
-
-            formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
-
             filename = "Console and File_Log" + str(time.time()) + ".log"
-
             stream_handler = logging.StreamHandler()
             stream_handler.setFormatter(formatter)
             file_handler = logging.FileHandler(filename, mode='a')
