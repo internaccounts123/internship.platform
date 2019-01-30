f2cfd7424eb51c3ae4e7c95e854cb6994b3b4e47
Asad Rizvi <asadimranrizvi@hotmail.com>
2019-01-30 12:53:08 +0500

    refactor(utility): added proper comments to the code

--- common/utility/conversions.py
+++ common/utility/conversions.py
@@ -2,10 +2,18 @@ import numpy as np
 M_PI = np.pi
 
 
-# This function converts an input angle from degree to radians
 def deg2rad(degree):
+    """
+    :param degree: Angle in Degrees
+    :return: Converted Angle in Radians
+    """
     return (degree / 180.0) * M_PI
 
 
 def rad2deg(angle):
+    """
+
+    :param angle: Angle in Radians
+    :return: Converted Angle in Degrees
+    """
     return angle * (180.0 / M_PI)
